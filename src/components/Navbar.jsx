d06b3b698480b0d39568cb768d2e79d63605fd37
sinharitun <ritunrajoff230301@gmail.com>
2025-04-24 23:31:54 +0530

    2.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 // Navbar.js
-
+import "./Navbar.css";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-black p-4">
+    <nav className="bg-black p-4 nav-main">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="/logo" className="text-lg font-bold text-white">
-          BloodBank.com
+        <a href="/" className="text-lg font-bold text-white">
+          Blood Bank
         </a>
 
         {/* Navigation Links */}
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 sub-nav">
           <a href="/" className="text-white hover:text-red-500 hover:underline">
             Home
           </a>
